33fbdb153b0be5f3e65517659b4506cb429d561b
Karina <milkokarina13@gmail.com>
2024-07-29 22:42:34 +0300

    Add current weather

--- src/App.jsx
+++ src/App.jsx
@@ -21,21 +21,33 @@
 // export default App;
 
 //---------------------------------------------------------------
-import Slider from "./components/Slider";
+// import Slider from "./components/Slider";
+
+// function App() {
+//   const imageUrls = [
+//     "https://cdn.esahubble.org/archives/images/wallpaper2/heic2017a.jpg",
+//     "https://cdn.esahubble.org/archives/images/thumb300y/potw2050a.jpg",
+//     "https://cdn.esahubble.org/archives/images/thumb300y/heic0506a.jpg",
+//     "https://cdn.esahubble.org/archives/images/thumb300y/potw2110a.jpg",
+//     "https://cdn.esahubble.org/archives/images/thumb300y/opo9828c.jpg",
+//     "https://cdn.esahubble.org/archives/images/thumb700x/opo0613a.jpg",
+//   ];
+
+//   return (
+//     <>
+//       <Slider imageUrls={imageUrls} />
+//     </>
+//   );
+// }
+
+// export default App;
+//---------------------------------------------------------------------
+import Weather from "./components/Weather";
 
 function App() {
-  const imageUrls = [
-    "https://cdn.esahubble.org/archives/images/wallpaper2/heic2017a.jpg",
-    "https://cdn.esahubble.org/archives/images/thumb300y/potw2050a.jpg",
-    "https://cdn.esahubble.org/archives/images/thumb300y/heic0506a.jpg",
-    "https://cdn.esahubble.org/archives/images/thumb300y/potw2110a.jpg",
-    "https://cdn.esahubble.org/archives/images/thumb300y/opo9828c.jpg",
-    "https://cdn.esahubble.org/archives/images/thumb700x/opo0613a.jpg",
-  ];
-
   return (
     <>
-      <Slider imageUrls={imageUrls} />
+      <Weather />
     </>
   );
 }
