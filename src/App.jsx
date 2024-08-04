445b2a7516f18542f65d2e2b331a5addda941a92
Karina <milkokarina13@gmail.com>
2024-08-04 23:37:24 +0300

    Update slider

--- src/App.jsx
+++ src/App.jsx
@@ -21,26 +21,26 @@
 // export default App;
 
 //---------------------------------------------------------------
-// import Slider from "./components/Slider";
+import Slider from "./components/Slider";
 
-// function App() {
-//   const imageUrls = [
-//     "https://cdn.esahubble.org/archives/images/wallpaper2/heic2017a.jpg",
-//     "https://cdn.esahubble.org/archives/images/thumb300y/potw2050a.jpg",
-//     "https://cdn.esahubble.org/archives/images/thumb300y/heic0506a.jpg",
-//     "https://cdn.esahubble.org/archives/images/thumb300y/potw2110a.jpg",
-//     "https://cdn.esahubble.org/archives/images/thumb300y/opo9828c.jpg",
-//     "https://cdn.esahubble.org/archives/images/thumb700x/opo0613a.jpg",
-//   ];
+function App() {
+  const imageUrls = [
+    "https://t4.ftcdn.net/jpg/02/66/31/75/240_F_266317554_kr7DPOoM5Uty0YCeFU9nDZTt4a2LeMJF.jpg",
+    "https://t3.ftcdn.net/jpg/01/03/31/48/240_F_103314840_pSOFixTmp0m48TfBFoYPw0pOpoaKebwJ.jpg",
+    "https://t4.ftcdn.net/jpg/04/15/79/09/240_F_415790935_7va5lMHOmyhvAcdskXbSx7lDJUp0cfja.jpg",
+    "https://t3.ftcdn.net/jpg/03/53/25/10/240_F_353251094_V7ANK1fZh03igu6EcB7zEskxqOrshlS3.jpg",
+    "https://t3.ftcdn.net/jpg/03/12/26/06/240_F_312260680_EVMXPzgpDj0F5FVoiJUbQ1axYmS5JArN.jpg",
+    "https://t4.ftcdn.net/jpg/02/69/18/47/240_F_269184784_4pnmyHf1jB4uXvoM0ggMNSWAOagpb8bW.jpg",
+  ];
 
-//   return (
-//     <>
-//       <Slider imageUrls={imageUrls} />
-//     </>
-//   );
-// }
+  return (
+    <>
+      <Slider imageUrls={imageUrls} />
+    </>
+  );
+}
 
-// export default App;
+export default App;
 //---------------------------------------------------------------------
 // import Weather from "./components/Weather";
 
@@ -81,10 +81,10 @@
 
 //-----------------------------------------------------
 
-import WeatherClass from "./components/WeatherClass";
+// import WeatherClass from "./components/WeatherClass";
 
-function App() {
-  return <WeatherClass />;
-}
+// function App() {
+//   return <WeatherClass />;
+// }
 
-export default App;
+// export default App;
--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import styles from "./Slider.module.sass";
+import classNames from "classnames";
 
 function Slider({ imageUrls }) {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const [isPaused, setIsPaused] = useState(true);
+  const [delay, setDelay] = useState("1000");
 
   const prevSlide = () => {
-    if (currentSlideIndex > 1) {
+    if (currentSlideIndex > 0) {
       setCurrentSlideIndex(
         (currentSlideIndex) =>
           (currentSlideIndex - 1 + imageUrls.length) % imageUrls.length
@@ -18,21 +21,38 @@ function Slider({ imageUrls }) {
       (currentSlideIndex) => (currentSlideIndex + 1) % imageUrls.length
     );
   };
-  console.log(currentSlideIndex);
+
+  const handlePlayStop = () => {
+    if (isPaused) {
+      setIsPaused(false);
+    } else {
+      setIsPaused(true);
+    }
+  };
+
+  useEffect(() => {
+    let id = null;
+    if (!isPaused) {
+      id = setTimeout(nextSlide, delay);
+    }
+    return () => {
+      clearInterval(id);
+    };
+  });
 
   return (
     <div className={styles.slideContainer}>
-      <h1 className={styles.slideHeader}>Top Images</h1>
+      <h1 className={styles.slideHeader}>Animal Images</h1>
       <img
         className={styles.imgSlides}
         src={imageUrls[currentSlideIndex]}
-        alt={`${currentSlideIndex + 1}`}
+        alt="animals"
       />
       <div className={styles.btnSlides}>
-        <button onClick={prevSlide} disabled={currentSlideIndex === 1}>
+        <button onClick={prevSlide} disabled={currentSlideIndex === 0}>
           {"<"}
         </button>
-        <span>{currentSlideIndex}</span>
+        <button onClick={handlePlayStop}>{isPaused ? "Play" : "Stop"}</button>
         <button
           onClick={nextSlide}
           disabled={currentSlideIndex === imageUrls.length - 1}
@@ -40,6 +60,14 @@ function Slider({ imageUrls }) {
           {">"}
         </button>
       </div>
+      Timer delay, ms:{" "}
+      <input
+        type="text"
+        placeholder="Enter the slide transition delay"
+        onChange={(e) => {
+          setDelay(e.target.value);
+        }}
+      />
     </div>
   );
 }
